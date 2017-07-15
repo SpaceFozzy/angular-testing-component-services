43b13ecef3af445e7c620358a6a152813242b854
Jon Richardson <jon@robotmonster.ca>
2017-07-15 14:34:48 -0400

    Add first basic tests with service stub

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { ExampleService } from './example.service'
+
+const exampleServiceStub = {
+
+}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +13,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [{ provide: ExampleService, useValue: exampleServiceStub }]
     }).compileComponents();
   }));
 
@@ -17,16 +23,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Mocking an Angular Service for Test');
   }));
 });
